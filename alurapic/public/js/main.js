447fafc901a7b02bfb176dc52bdd97c1a8e5eef6
Paulo Santos <paulo.bruno.ss@gmail.com>
2018-02-11 13:06:21 -0200

    Dividido html parciais e configurado rotas

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,14 @@
-angular.module('alurapic', ['minhasDiretivas']);
+angular.module('alurapic', ['minhasDiretivas', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/fotos',{
+        templateUrl: 'partial/principal.html',
+        controller: 'FotosController'
+    });
+    $routeProvider.when('/fotos/new',{
+        templateUrl: 'partial/foto.html',
+    });
+    $routeProvider.otherwise({ redirectTo: '/fotos'});
+});
