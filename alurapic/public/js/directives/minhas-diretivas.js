43958a33b155b1f26bc94af669d9acd1389a9235
paulobrunoss <paulo.bruno.ss@gmail.com>
2018-02-11 11:51:51 -0200

    Add files via upload

---
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -0,0 +1,16 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function(){
+    var ddo = {}
+
+    ddo.restrict = 'AE'
+
+    ddo.scope = {
+        titulo: '@titulo'
+    }
+
+    ddo.transclude = true;
+
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+    
+    return ddo;
+});
